257f4643fe091b5b93e4f7bd2324de63c22edba8
objected <ma.weiyi@dhs.sg>
2022-05-13 15:26:16 +0800

    add adjacency list graph input for main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 int main() {
-    cout << "Hello World!\n";
+    int n, e; 
+    cout << "Input number of nodes: "; cin >> n;
+    cout << "Input number of edges: "; cin >> e;
+
+    vector<pair<int, int>> g[e+5];
+
+    cout << "For the following prompts, input start node, end node, and weight (space-separated).";
+
+    for (int i = 1; i <= e; i++) {
+        int a, b, w; 
+        cout << "Edge " << i << ": ";
+        cin >> a >> b >> w;
+
+        g[a].push_back(make_pair(b, w));
+        g[b].push_back(make_pair(a, w));
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << i << ": ";
+        for (auto x: g[i]) {
+            cout << "(to " << x.first << ", travel " << x.second << ") ";
+        }
+        cout << '\n';
+    }
+
     return 0;
 }
